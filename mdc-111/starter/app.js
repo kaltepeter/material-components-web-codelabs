df8281135462c75731bbe5e0df7a57a367f712ea
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-31 14:47:49 -0500

    111: using material web components

--- mdc-111/starter/app.js
+++ mdc-111/starter/app.js
@@ -1,5 +1,19 @@
-const shippingForm = document.querySelector('#crane-shipping-form');
-shippingForm.addEventListener('submit', (evt) => {
+import { MDCRipple } from "@material/ripple";
+import { MDCSelect } from "@material/select";
+import { MDCTextField } from "@material/textfield";
+
+new MDCRipple(document.querySelector(".mdc-button"));
+new MDCSelect(document.querySelector(".mdc-select"));
+
+const textFieldElements = [].slice.call(
+  document.querySelectorAll(".mdc-text-field")
+);
+textFieldElements.forEach(textFieldEl => {
+  new MDCTextField(textFieldEl);
+});
+
+const shippingForm = document.querySelector("#crane-shipping-form");
+shippingForm.addEventListener("submit", evt => {
   evt.preventDefault();
-  alert('Success!');
+  alert("Success!");
 });
